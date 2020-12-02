4366011d2220367a4a5a979a2171fcd7cef289ce
Gutenson <1517780819121002@mil>
2020-12-02 10:15:50 -0600

    Now sorting stations based upon distance from primary station.

--- arc/anteProcess.py
+++ arc/anteProcess.py
@@ -878,11 +878,34 @@ class Main(object):
                 self.log.Wrap(traceback.format_exc())
         # Maintain processing pool until all jobs are complete and collect results
         self.finish_multiprocessing(tasks_queue, results_queue, minions, enqueue_count)
-        # Sort stations by weighted difference
+
+        # find the primary station after multiprocessing finishes
+        need_primary = True
+        primary_station = self.getBest(need_primary=need_primary)
+        print(primary_station.location)
+        secondary_stations_sorted_list = []
+        if primary_station is not None:
+            # Note that the primary station has been found
+            if need_primary is True:
+                need_primary = False
+        # If stations are not the primary station, sort stations by weighted difference
         sorted_stations = []
         for station in self.stations:
-            sorted_stations.append([station.weightedDiff, station])
+            # loop through non-primary-stations and recalculate distance, etc.
+            # based off of distance from primary station
+            if station.name != primary_station.name:
+                distance = great_circle(primary_station.location, station.location).miles
+                distance = round(distance, 3)
+                station.distance = distance
+                elevDiff = round((primary_station.elevation - station.elevation), 3)
+                elevDiff = abs(elevDiff)
+                station.elevDiff = elevDiff
+                weightedDiff = round(distance*((elevDiff/1000)+0.45), 3)
+                station.weightedDiff = weightedDiff
+                sorted_stations.append([station.weightedDiff, station])
         sorted_stations.sort(key=lambda x: x[0], reverse=False)
+        # insert primary station at the top of the list
+        sorted_stations.insert(0, [primary_station.weightedDiff, primary_station])
         self.stations = []
         self.log.Wrap('Looking for stations missing data...')
         for sort_list in sorted_stations:
@@ -1006,34 +1029,36 @@ class Main(object):
         # Fill in NaN using top station
         n = 0
         num_stations_used = 0
-        need_primary = True
         if float(self.site_lat) < 50:
             maxSearchDistance = 60      # Maximum distance between observation point and station location
         else: # In AK, where stations are very rare
             maxSearchDistance = 300
         maxNumberOfStations = 15    # Maximum number of stations to use to complete record
         while self.finalDF.isnull().sum().sum() > 0 and num_stations_used < maxNumberOfStations and self.searchDistance <= maxSearchDistance:
-            n += 1
-            if n == 1:
-                self.log.Wrap(str(self.finalDF.isnull().sum().sum()) + ' null values.')
-            if self.searchDistance > 60:
-                need_primary = False
-            best_station = self.getBest(need_primary=need_primary)
-            if best_station is not None:
-                # Note that the primary station has been found
-                if need_primary is True:
+            for station in self.stations:
+                print(station)
+                n += 1
+                if n == 1:
+                    self.log.Wrap(str(self.finalDF.isnull().sum().sum()) + ' null values.')
+                if self.searchDistance > 60:
                     need_primary = False
-                    primary_station = best_station # added by JLG
-                # Get baseline null value count
+                # best_station = self.getBest(need_primary=need_primary)
+                # if best_station is not None:
+                    # Note that the primary station has been found
+                    # if need_primary is True:
+                    #     need_primary = False
+                    #     primary_station = best_station # added by JLG
+                        # recalcuate and set source
+                    # Get baseline null value count
                 missing_before_normal = self.finalDF[self.dates.normal_period_data_start_date:self.dates.normal_period_end_date].isnull().sum().sum()
                 missing_before_antecedent = self.finalDF[self.dates.antecedent_period_start_date:self.dates.observation_date].isnull().sum().sum()
-                values = best_station.Values
-                self.log.Wrap('Attempting to replace null values with values from {}...'.format(best_station.name))
+                values = station.Values
+                self.log.Wrap('Attempting to replace null values with values from {}...'.format(station.name))
                 # Fill
                 try:
                     self.finalDF.fillna(values, inplace=True)
                 except ValueError:
-                    self.log.Wrap('ERROR: No values found for {}. Station will be skipped.'.format(best_station.name))
+                    self.log.Wrap('ERROR: No values found for {}. Station will be skipped.'.format(station.name))
                 missingAfter = self.finalDF.isnull().sum().sum()
                 self.log.Wrap(str(missingAfter) + ' null values remaining.')
                 missing_after_normal = self.finalDF[self.dates.normal_period_data_start_date:self.dates.normal_period_end_date].isnull().sum().sum()
@@ -1042,16 +1067,16 @@ class Main(object):
                 num_rows_antecedent = missing_before_antecedent-missing_after_antecedent
                 num_rows = num_rows_normal + num_rows_antecedent
                 if num_rows > 0:
-                    if best_station.name == primary_station.name:
+                    if n == 1: # n=1 should be the primary station in the station list
                         num_stations_used += 1
                         # BUILD STATIONS TABLE
                         vals = []
-                        vals.append(best_station.name)
-                        vals.append(best_station.location)
-                        vals.append(best_station.elevation)
-                        vals.append("{0}*".format(best_station.distance))
-                        vals.append("{0}*".format(best_station.elevDiff))
-                        vals.append("{0}*".format(best_station.weightedDiff))
+                        vals.append(station.name)
+                        vals.append(station.location)
+                        vals.append(station.elevation)
+                        vals.append("{0}*".format(station.distance))
+                        vals.append("{0}*".format(station.elevDiff))
+                        vals.append("{0}*".format(station.weightedDiff))
                         vals.append(num_rows_normal)
                         vals.append(num_rows_antecedent)
                         station_table_values.append(vals)
@@ -1059,16 +1084,12 @@ class Main(object):
                         num_stations_used += 1
                         # BUILD STATIONS TABLE
                         vals = []
-                        distance = great_circle(primary_station.location, best_station.location).miles
-                        distance = round(distance, 3)
-                        elevDiff = round((primary_station.elevation - best_station.elevation), 3)
-                        weightedDiff = round(distance*((elevDiff/1000)+0.45), 3)
-                        vals.append(best_station.name)
-                        vals.append(best_station.location)
-                        vals.append(best_station.elevation)
-                        vals.append(distance)
-                        vals.append(elevDiff)
-                        vals.append(weightedDiff)
+                        vals.append(station.name)
+                        vals.append(station.location)
+                        vals.append(station.elevation)
+                        vals.append(station.distance)
+                        vals.append(station.elevDiff)
+                        vals.append(station.weightedDiff)
                         vals.append(num_rows_normal)
                         vals.append(num_rows_antecedent)
                         station_table_values.append(vals)
@@ -1076,14 +1097,14 @@ class Main(object):
                     if self.save_folder is not None:
                         # Generate output
                         try:
-                            station_csv_name = '{}.csv'.format(best_station.name).replace('/','_') # Slashes keep getting added to file names somehow, causing failures here
+                            station_csv_name = '{}.csv'.format(station.name).replace('/','_') # Slashes keep getting added to file names somehow, causing failures here
                             station_csv_path = os.path.join(self.stationFolderPath, station_csv_name)
                             if os.path.isfile(station_csv_path) is False:
                                 self.log.Wrap('Saving station data to CSV in output folder...')
-                                best_station.Values.to_csv(station_csv_path)
+                                station.Values.to_csv(station_csv_path)
                         except Exception:
                             pass # Will add an announcement that station data save failed later
-            else:
+            if self.finalDF.isnull().sum().sum() > 0:
                 self.log.Wrap("")
                 self.log.Wrap("No suitable station available to replace null values.")
                 if float(self.site_lat) < 50:
@@ -1539,7 +1560,7 @@ class Main(object):
         fig.set_dpi(140)
         # add a footer to the station table to describe the asterisk.
         footer_text = '*This station is considered the primary station and these values are generated in relation to the location of interest.'
-        fig.text(0.51,0.18, footer_text, fontsize=10, color="black")
+        fig.text(0.51,0.31, footer_text, fontsize=10, color="black")
         if self.data_type == 'PRCP':
         #    if num_stations_used < 14:
             ax1 = plt.subplot2grid((9, 10), (0, 0), colspan=10, rowspan=6)
